3da62a192e824ec8d4442bd0d0191f1a78a6d3de
Alok <kashi560@gmail.com>
2022-03-12 17:12:22 +0530

    settings updated1

--- music/settings.py
+++ music/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ga6g2$4%2(v98*nds7eho^)i!lp&dqgw+jp5k5^7ufedf7)5h3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['music-poc-ak.herokuapp.com']
 
 
 # Application definition
